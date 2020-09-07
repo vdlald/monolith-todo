21c32fb3c1d216671d027bb8cca6de17fd3aaf3f
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-09-07 11:53:31 +0300

    add UserService

---
+++ src/main/java/com/vladislav/todo/services/UserService.java
@@ -0,0 +1,22 @@
+package com.vladislav.todo.services;
+
+import com.vladislav.todo.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Not found user with username: " + username));
+    }
+}
