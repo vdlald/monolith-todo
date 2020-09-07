c3dd7af683dc94507140537bd3f5b3fa49fa1ef8
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-09-07 12:10:26 +0300

    add TaskService

---
+++ src/main/java/com/vladislav/todo/services/TaskService.java
@@ -0,0 +1,14 @@
+package com.vladislav.todo.services;
+
+import com.vladislav.todo.repositories.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+}
